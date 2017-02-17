88bcdc306226fff3d3ff05cf459ccfa455dae79a
bernulander <bernulander@gmail.com>
2017-02-16 16:21:49 -0800

    remove ajax, rollback to simple understandable code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,36 +1,36 @@
 class CommentsController < ApplicationController
 
-# def create
-#   comment_params    = params.require(:comment).permit(:body)
-#   @comment          = Comment.new comment_params
-#   @post = Post.find params[:post_id]
-#   @comment.post = @post
-#
-#   if @comment.save
-#     redirect_to post_path(params[:post_id]), notice: 'comment created!'
-#   else
-#     flash[:alert] = 'please fix errors'
-#     render 'posts/show'
-#   end
-# end
-
-# ajax version
 def create
   comment_params    = params.require(:comment).permit(:body)
   @comment          = Comment.new comment_params
   @post = Post.find params[:post_id]
   @comment.post = @post
 
-  respond_to do |format|
-    if @comment.save
-      format.html {redirect_to post_path(params[:post_id]), notice: 'comment created!'}
-      format.js
-    else
-      # flash[:alert] = 'please fix errors'
-      # format.html
-    end
+  if @comment.save
+    redirect_to post_path(params[:post_id]), notice: 'comment created!'
+  else
+    flash[:alert] = 'please fix errors'
+    render 'posts/show'
   end
 end
+
+# ajax version
+# def create
+#   comment_params    = params.require(:comment).permit(:body)
+#   @comment          = Comment.new comment_params
+#   @post = Post.find params[:post_id]
+#   @comment.post = @post
+#
+#   respond_to do |format|
+#     if @comment.save
+#       format.html {redirect_to post_path(params[:post_id]), notice: 'comment created!'}
+#       format.js
+#     else
+#       # flash[:alert] = 'please fix errors'
+#       # format.html
+#     end
+#   end
+# end
 
 def destroy
   comment = Comment.find params[:id]
